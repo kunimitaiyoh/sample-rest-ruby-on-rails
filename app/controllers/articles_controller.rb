a5f04d3aea0848f7db97491be849518dee64d442
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-03-25 19:09:16 +0900

    create actions "edit", "update" for controller articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,16 @@
 class ArticlesController < ApplicationController
   def new
-    
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -18,7 +21,22 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
-  private def article_params
-    params.require(:article).permit(:title, :text)
+  def edit
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end

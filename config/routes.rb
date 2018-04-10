6e0a803e9b21b83112856cc183a41da26ea27b72
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-04-10 20:53:01 +0900

    add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
